683990b3cb4b20514ea639d62689681a02c98a9e
Choll Song <chollsong212@gmail.com>
2016-12-12 21:33:39 +0700

    commit before additional changes

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
 /**
  * Created by Choll on 12/12/2016.
  */
-var app = require('express')();
+var express = require('express');
+var mongodb = require('mongodb');
+var app = express();
+var port = 3000;
+var mongoUrl = 'mongodb://localhost:27017/infosys';
+
+app.get('/', function (req, res) {
+    res.send('Hello World');
+});
+
+
+console.log("Listening on port: "+port);
+app.listen(port);
