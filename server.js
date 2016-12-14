aac603b5f1d12d6899cd9d86d4f5bbd0f407e394
sasinn98 <sasinn98@gmail.com>
2016-12-14 22:34:25 +0700

    formatted server

--- server.js
+++ server.js
@@ -14,94 +14,87 @@ var MongoClient = mongodb.MongoClient;
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-    extended : true
-}));
- app.use(express.static(__dirname));
-
-
+		extended: true
+	}));
+app.use(express.static(__dirname));
 
 app.get('/', function (req, res) {
-    console.log("has something coming from get");
-    res.sendFile(__dirname + '/index.html');
+	console.log("has something coming from get");
+	res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/v/areas', function (req, res) {
+	//for testing
+	// console.log("has something coming");
+	// console.log(req.query.subArea);
+	// res.send(JSON.stringify({ a: 1 }));
+	MongoClient.connect(mongoUrl, function (err, db) {
+		if (err) {
+			console.log(err);
+			res.send({
+				'status': 0,
+				'error': err
+			})
+		} else {
+			var collection = db.collection('subAreas');
+			collection.findOne({
+				'subArea': parseInt(req.query.subArea)
+			}, function (err, doc) {
+				if (err) {
+					return res.send({
+						'status': 0,
+						'error': err
+					});
+				} else if (doc != null) {
+					console.log('Found ' + req.query.subArea + '.');
+					return res.send({
+						'status': 0,
+						'area_name': doc.name,
+						'information': doc.information,
+						'seat_available': doc.seat_available
 
-
-
-app.get('/v/areas', function(req,res){
-    //for testing
-    // console.log("has something coming");
-    // console.log(req.query.subArea);
-    // res.send(JSON.stringify({ a: 1 }));
-    MongoClient.connect(mongoUrl, function (err, db) {
-        if(err){
-            console.log(err);
-            res.send({
-                'status':0,
-                'error':err
-            })
-        }else{
-            var collection = db.collection('subAreas');
-            collection.findOne({
-                'subArea' : parseInt(req.query.subArea)
-            }, function (err, doc) {
-                if (err) {
-                    return res.send({
-                        'status' : 0,
-                        'error' : err
-                    });
-                } else if (doc != null) {
-                    console.log('Found ' + req.query.subArea + '.');
-                    return res.send({
-                        'status' : 0,
-                        'area_name' : doc.name,
-                   'information': doc.information,
-                    'seat_available': doc.seat_available
-
-                    });
-                } else {
-                    console.log('Can not find ' + req.query.subArea + '.');
-                    return res.send({
-                        'status' : 0,
-                        'error' : "not found"
-                    });
-                }
-                db.close();
-            });
-        }
-    });
+					});
+				} else {
+					console.log('Can not find ' + req.query.subArea + '.');
+					return res.send({
+						'status': 0,
+						'error': "not found"
+					});
+				}
+				db.close();
+			});
+		}
+	});
 });
 
 //for testing ignore
 
-app.post('/test', function(req, res){
-    MongoClient.connect(mongoUrl, function (err, db) {
-        if(err){
-            console.log(err);
-            res.send({
-                error:0,
-                msg:"db error"
-            })
-        }else{
-            var collection = db.collection('testCollection');
-            var user = {
-                name : '',
-                data : 50
-            };
-            collection.insert(user, function (err, doc) {
-               if(err){
-                   console.log("error has occured");
-               } else{
-                   console.log("input successful");
-               }
-            });
-        }
-    });
+app.post('/test', function (req, res) {
+	MongoClient.connect(mongoUrl, function (err, db) {
+		if (err) {
+			console.log(err);
+			res.send({
+				error: 0,
+				msg: "db error"
+			})
+		} else {
+			var collection = db.collection('testCollection');
+			var user = {
+				name: '',
+				data: 50
+			};
+			collection.insert(user, function (err, doc) {
+				if (err) {
+					console.log("error has occured");
+				} else {
+					console.log("input successful");
+				}
+			});
+		}
+	});
 
 });
 
-
-
 http.listen(port, function () {
-    console.log("Listening on port: "+port);
+	console.log("Listening on port: " + port);
 });
